3f6c4423bde20adb384df93dbe79a2253354cb5e
Satinder Singh <92389558+Rednitas10@users.noreply.github.com>
2022-10-10 11:59:35 -0700

    Update Mineweeper2.java
    this can be used as the group assignment official solution

--- src/Mineweeper2.java
+++ src/Mineweeper2.java
@@ -1,149 +1,123 @@
+/**
+ * @author An Nguyen, Satinder Singh, Xuan Dao
+ * @version 10/10/2022
+ * Professor Tom Capaul
+ * Group Project 1
+ */
+
+
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.Scanner;
+/**
+ *  This class takes text from System to generate a 2D array containing
+ *  mines and numbers indicating how many mines are nearby cells
+ *
+ */
 
-// java src/Mineweeper2.java test_input.txt
+public class Minesweeper2 {
+    /*
+     * This method takes in a String array of argument
+     * @param String[] called theArgs
+     * @return void
+     */
 
-public class Mineweeper2 {
-    public static void main(String[] args) throws FileNotFoundException {
-        String str_output = "";
-        File file;
-
-        if(args.length > 0)
-            file = new File(args[0]);
-        else
-            file = new File("minesweeper_input.txt");
-
-        Scanner scan = new Scanner(file);
-
-        int cnt_field = 0;
+    public static void main(String[] theArgs) throws FileNotFoundException {
+        Scanner scan = new Scanner(System.in);
+        if (theArgs.length == 1){
+            scan = new Scanner(new File(theArgs[0]));
+        }
+        int countField = 0;
         while (scan.hasNext()) {
-            //String firstLine = scan.nextLine();
             int row = scan.nextInt();
             int col = scan.nextInt();
-            char[][] matrix_mines = new char[row][col];
-            cnt_field +=1;
-            if(row == 0 && col == 0){
-                str_output += "End of program.";
-                System.out.println("End of program.");
+            char[][] matrixMines = new char[row][col];
+
+            if(row == 0){
                 break;
             }
-
+            countField += 1;
             scan.nextLine(); // reads "enter" from keyboard
             for (int i = 0; i < row; i++) {
                 String str = scan.nextLine();
                 for (int j = 0; j < col; j++) {
-                    matrix_mines[i][j] = str.charAt(j);
+                    matrixMines[i][j] = str.charAt(j);
                 }
             }
 
-            //output Field
-            str_output += "Field #" + cnt_field +":\n";
-            System.out.println("Field #" + cnt_field +":");
-            //print2D(matrix_mines);
+            System.out.println("Field #" + countField +":");
+            //print2D(matrixMines);
 
             //translate matrix
-           char[][] matrix_converted = translateMatrix(matrix_mines);
-           str_output += print2D(matrix_converted) + "\n";
-        }
-
-       /* if(args.length > 1)
-            outputFile(str_output, args[1]);
-        else
-            outputFile(str_output, "test_output.txt");*/
-
-        //write output file
-        try {
-            File output;
-            FileWriter myWriter;
-            if(args.length > 1){
-                output = new File(args[0]);
-                myWriter = new FileWriter(output);
-            }
-            else {
-                output = new File("test_output.txt");
-                myWriter = new FileWriter(output);
-            }
-            myWriter.write(str_output);
-            myWriter.close();
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
+            char[][] matrixConverted = translateMatrix(matrixMines);
+            System.out.println(print2D(matrixConverted));
         }
 
     }
 
-    public static String print2D(char matrix[][]) {
-        String str = "";
-        for (int i = 0; i < matrix.length; i++) {
-            // Loop through all elements of current row
-            for (int j = 0; j < matrix[i].length; j++) {
-                str += matrix[i][j];
-                //System.out.print(matrix[i][j]);
-            }
-            str += "\n";
+    /*
+     *  This method prints the 2D array as a String in the correct format
+     *  @param Takes in a 2D array of char
+     *  @return String conversion of char in the array
+     */
+     public static String print2D(final char theMatrix[][]) {
+         String str = "";
+         for (int i = 0; i < theMatrix.length; i++) {
+             // Loop through all elements of current row
+             for (int j = 0; j < theMatrix[i].length; j++) {
+                 str += theMatrix[i][j];
+                 //System.out.print([i][j]);
+             }
+             str += "\n";
 
-        }
-        System.out.print(str);
-        return str;
-    }
+         }
+         System.out.print(str);
+         return str;
+     }
+    /*
+     *  This method translate the 2D array from '*' and '.' to '*' and a number
+     *  that tells how many mines are located adjacent to the cell
+     *  @param Takes in a 2D array of char
+     *  @return a 2D array of converted chars
+     */
+    public static char[][] translateMatrix(final char theMatrix[][]) {
+        int count;
+        char matrixConverted[][] = new char[theMatrix.length][theMatrix[0].length];
+        for (int i = 0; i < theMatrix.length; i++) {
+            for (int j = 0; j < theMatrix[i].length; j++) {
+                count = 0;
+                if (i - 1 >= 0 && theMatrix[i - 1][j] == '*') { //north
+                    count += 1;
+                }
+                if (i + 1 < theMatrix.length && theMatrix[i + 1][j] == '*') { // south
+                    count += 1;
+                }
 
-    public static char[][] translateMatrix(char matrix[][]) {
-        int cnt_mines = 0;
-        int cnt;
-        char matrix_converted[][] = new char[matrix.length][matrix[0].length];
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                cnt = 0;
-                if (i - 1 >= 0 && matrix[i - 1][j] == '*') { //north
-                    cnt += 1;
+                if ( j + 1 < theMatrix[i].length && theMatrix[i][j + 1] == '*') { //east
+                    count += 1;
                 }
-                if (i + 1 < matrix.length && matrix[i + 1][j] == '*') { // south
-                    cnt += 1;
+                if (j - 1 >= 0 && theMatrix[i][j - 1] == '*') { //west
+                    count += 1;
                 }
-
-                if ( j + 1 < matrix[i].length && matrix[i][j + 1] == '*') { //east
-                    cnt += 1;
+                if ((i - 1 >= 0 && j + 1 < theMatrix[i].length) && theMatrix[i - 1][j + 1] == '*') { //north-east
+                    count += 1;
                 }
-                if (j - 1 >= 0 && matrix[i][j - 1] == '*') { //west
-                    cnt += 1;
+                if ((i - 1 >= 0 && j - 1 >= 0) && theMatrix[i - 1][j - 1] == '*') { //north-west
+                    count += 1;
+                } if ((i + 1 < theMatrix.length && j + 1 < theMatrix[i].length) && theMatrix[i + 1][j + 1] == '*') { //south-east
+                    count += 1;
+                } if ((i + 1 < theMatrix.length && j - 1 >= 0) && theMatrix[i + 1][j - 1] == '*') { //south-west
+                    count += 1;
                 }
-                if ((i - 1 >= 0 && j + 1 < matrix[i].length) && matrix[i - 1][j + 1] == '*') { //north-east
-                    cnt += 1;
-                }
-                if ((i - 1 >= 0 && j - 1 >= 0) && matrix[i - 1][j - 1] == '*') { //north-west
-                    cnt += 1;
-                } if ((i + 1 < matrix.length && j + 1 < matrix[i].length) && matrix[i + 1][j + 1] == '*') { //south-east
-                    cnt += 1;
-                } if ((i + 1 < matrix.length && j - 1 >= 0) && matrix[i + 1][j - 1] == '*') { //south-west
-                    cnt += 1;
-                }
-                if(matrix[i][j] == '*'){
-                    matrix_converted[i][j] = '*';
+                if(theMatrix[i][j] == '*'){
+                    matrixConverted[i][j] = '*';
                 }
                 else {
-                    matrix_converted[i][j] = (char) (cnt + '0');
+                    matrixConverted[i][j] = (char) (count + '0');
                 }
 
             }
         }
-        return matrix_converted;
+        return matrixConverted;
     }
-
-    public static void outputFile (String str, String filename){
-        try {
-            File output = new File(filename);
-            FileWriter myWriter = new FileWriter(output);
-            myWriter.write(str);
-            myWriter.close();
-            //System.out.println(str);
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-    }
-
-
 }
